d86e19d460cde2fa8c11d6d905a2006640f985c1
Trisha Dawn <trisha@tamu.edu>
2021-11-19 19:31:32 -0600

    Create R file for detecting multiple change-points (number known) and add extra input to consider how many of them to detect

---
+++ R/MultipleChangePoint_NumKnown.R
@@ -0,0 +1,65 @@
+# Function to detect Single Change Point when it is known that only one change point exists
+
+# Inputs:
+# Data matrix X (n X d) : whose rows are the d dimensional observations
+# dist.method : Linkage method to be used in the hierarchical clustering (single, average or complete)
+# numcp : Number of change-points to be detected (should be more than 1)
+
+# Output:
+# detectcp: Detected location vector for detected multiple change-points
+detect_single_cp = function(X, numcp, dist.method)
+{
+  # Number of observations
+  n = nrow(X)
+
+  # Calculate distance matrix
+  dist_mat = as.matrix(dist(X), method = "euclidean")
+
+  # Initialize the matrix with zeroes which will monitor merging of clusters
+  trackclust_mat = matrix(0, n, n)
+
+  # At initial stage one observation corresponds to one cluster
+  trackclust_mat[1, ] = 1:n
+
+  # Running loop till n-1 (since we need pairwise distances)
+  for(j in 1:(n-1))
+  {
+    # Since at each stage the number of clusters decreases by 1
+    l = n - j
+
+    # looking at current stage of number of clusters
+    current_track = trackclust_mat[j, ]
+
+    # Calculating linkage distances
+    link_dist = sapply(1:l, function(i) {
+
+      # Considering two clusters at a time
+      # One cluster index
+      clust_index_1 = current_track[i]:(current_track[i+1]-1)
+      # Immediate next cluster index
+      clust_index_2 = current_track[i+1]:(ifelse( i < l, current_track[i+2] - 1, n))
+
+      # Distance between the two consecutive clusters
+
+      if(dist.method == "single")
+      {
+        # return single linkage value
+        return(min(dist_mat[clust_index_1,clust_index_2]))
+      } else if(dist.method == "average") {
+        # return average linkage value
+        return(mean(dist_mat[clust_index_1,clust_index_2]))
+      }else{
+        # return complete linkage value
+        return(max(dist_mat[clust_index_1,clust_index_2]))
+      }
+    })
+
+    # detecting which clusters are closest
+    current_changepoint = which.min(distvec)
+
+    # Merging the clusters and storing the numbers from which only new clusters start
+    trackclust_mat[j+1, ] = c(current_track[-(current_changepoint + 1)], 0)
+  }
+
+
+}
