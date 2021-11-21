f62550c109e7920cc88cf8fa727bda14f3e5b492
Trisha Dawn <trisha@tamu.edu>
2021-11-21 17:11:14 -0600

    Add check for multiple change point code

--- R/Tests.R
+++ R/Tests.R
@@ -11,3 +11,16 @@ test1 = function(a)
 }
 
 test1("wgt")
+
+d = 5
+X1 = mvtnorm::rmvnorm(10, mean = rep(0, d), sigma = diag(d))
+X2 = mvtnorm::rmvnorm(10, mean = rep(4, d), sigma = diag(d))
+X3 = mvtnorm::rmvnorm(10, mean = rep(8, d), sigma = diag(d))
+X = rbind(X1, X2, X3)
+
+detect_multiple_cp(X = X, numcp = 2, dist.method = "average")
+
+#source("SingleChangePoint.R")
+
+#source("MultipleChangePoint_NumKnown.R")
+
