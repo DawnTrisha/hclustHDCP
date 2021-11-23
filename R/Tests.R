abb0a1884bd42212acc1fc67b68c747d272fd067
Trisha Dawn <trisha@tamu.edu>
2021-11-22 21:01:29 -0600

    Modify test example for Multiple change-point

--- R/Tests.R
+++ R/Tests.R
@@ -13,14 +13,14 @@ test1 = function(a)
 test1("wgt")
 
 d = 5
-X1 = mvtnorm::rmvnorm(10, mean = rep(0, d), sigma = diag(d))
-X2 = mvtnorm::rmvnorm(10, mean = rep(4, d), sigma = diag(d))
-X3 = mvtnorm::rmvnorm(10, mean = rep(8, d), sigma = diag(d))
+X1 = matrix(rnorm((10 * 5), mean = 0, sd = 1), nrow = 10, ncol = d)
+X2 = matrix(rnorm((10 * 5), mean = 4, sd = 1), nrow = 10, ncol = d)
+X3 = matrix(rnorm((10 * 5), mean = 8, sd = 1), nrow = 10, ncol = d)
 X = rbind(X1, X2, X3)
 
 detect_multiple_cp(X = X, numcp = 2, dist.method = "average")
-
-#source("SingleChangePoint.R")
-
-#source("MultipleChangePoint_NumKnown.R")
+#
+# source("SingleChangePoint.R")
+#
+# source("MultipleChangePoint_NumKnown.R")
 
