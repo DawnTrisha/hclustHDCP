1dc40855de4888f8b33153f4868bacb7c611d93f
Trisha Dawn <trisha@tamu.edu>
2021-12-04 11:36:18 -0600

    Code to calculate Generalized distance matrix and use one of them via options

--- R/Distances.R
+++ R/Distances.R
@@ -89,9 +89,9 @@ gen_MADD_dist = function(n, u, v, Y)
     d2 = 0
 
     # Generalized Distance between u and other observations
-    d1 = gen_f(u, Y[k, ])
+    d1 = gen_f(u, Y[k, ], length(u))
     # Generalized Distance between v and other observations
-    d2 = gen_f(v, Y[k, ])
+    d2 = gen_f(v, Y[k, ], length(v))
 
     # Sum of absolute differences
     temp = temp + abs(d1 - d2)
@@ -102,4 +102,53 @@ gen_MADD_dist = function(n, u, v, Y)
 
   # Returning the distance
   return(as.vector(dist))
+}
+
+# Calculation of Generalized MADD distance matrix
+genMADD_distmat = function(X)
+{
+  # Number of observations
+  n = nrow(X)
+
+  # Initialization of distance matrix to which value will be stored
+  dist_matrix = array(0, dim = c(n,n))
+
+  # For any observation
+  for(i in 1:(n-1))
+  {
+    # Fix that observations
+    u = X[i,]
+
+    # Run the loop for all the other observations
+    for(j in (i+1):n)
+    {
+      # Other observations
+      v = X[j,]
+      # Other observations eliminating the two chosen observations
+      Y = X[-c(i,j),]
+
+      # Call the MADD_dist function between the two observations
+      dist_matrix[i,j] = (gen_MADD_dist(n, u, v, Y))^2
+      # Since symmetric
+      dist_matrix[j,i] = dist_matrix[i,j]
+    }
+  }
+
+  # Return distance matrix
+  return(dist_matrix)
+}
+
+# To construct distance matrix using MADD or generalized MADD
+# MADD distance or Generalized MADD distance matrix to accommodate change in
+# higher order marginals in High Dimensional Low Sample Size (HDLSS) situation
+distmat_HDLSS = function(X, option = c("genMADD","MADD"))
+{
+  option = match.arg(option)
+
+  # Asks user if want to use MADD or generalized MADD
+  if(option == "genMADD"){
+    return(genMADD_distmat(X))
+  }else{
+    return(MADD_distmat(X))
+  }
 }
